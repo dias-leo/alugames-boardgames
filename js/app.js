ac8d9086e1590de619b683b44ed5cbba98f2038c
Leonardo Dias <leonardodiasdasilva1990@gmail.com>
2024-07-16 22:49:11 -0300

    projeto boardgames

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if (botao.classList)
+
+    alert(nomeJogo.textContent);
+}
